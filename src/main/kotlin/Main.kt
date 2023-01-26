0be719ca962db4b99f7aaf7a5526b4a11e7b90db
Abbas alsbihawi <abbas202559@gmail.com>
2023-01-27 00:23:05 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+    println("Hello World!")
+    var numberText :String? ="null"
+    var numberCHar :Char? ='9'
+    println("number :${numberText?.length}")
+    numberText.run {
+        println("abbas")
+    }
+//    numberText.length
+//    Mans().isMan()
+}
+
+//class Mans :Man{
+//    override fun isMan() {
+//       println("is Man")
+//    }
+//
+//}
+//
+//interface Man{
+//    fun  isMan();
+//}
