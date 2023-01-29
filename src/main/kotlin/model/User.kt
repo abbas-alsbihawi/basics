644ad0d91b20cebfddf42eea6e4aee8352aef840
Abbas alsbihawi <abbas202559@gmail.com>
2023-01-29 19:56:13 +0300

    data class

--- src/main/kotlin/model/User.kt
+++ src/main/kotlin/model/User.kt
@@ -1,21 +1,24 @@
 package model
 
-class User( var username:String,var password:String,var phone:String,var city:String) {
-    var conter:String?=null
-       public get
-       private set(value) {
-           field = if (value!!.length>4)
-               value
-           else ""
-       }
 
-    init {
-          username="abbas"
-        counterUser++
-    }
-
-    companion object{
-        var counterUser=0
-    }
-
-}
+data class User( var username:String,var password:String,var phone:String,var city:String)
+//
+//class User( var username:String,var password:String,var phone:String,var city:String) {
+//    var conter:String?=null
+//       public get
+//       private set(value) {
+//           field = if (value!!.length>4)
+//               value
+//           else ""
+//       }
+//
+//    init {
+//          username="abbas"
+//        counterUser++
+//    }
+//
+//    companion object{
+//        var counterUser=0
+//    }
+//
+//}
